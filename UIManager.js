0825f1f772fdc31f49c12788d8ccb4d86ff0f0bc
ahmedbr1 <ahmed.sherif0120055942@gmail.com>
2025-04-04 21:43:27 +0200

    whatever

--- UIManager.js
+++ UIManager.js
@@ -3,11 +3,11 @@ export class UIManager {
     constructor(scene, gameManager) {
       this.scene = scene;
       this.gameManager = gameManager;
-      this.perks = [false, false, false];
       this.fishButton = null;
       this.resetButton = null;
       this.tutButton = null
       this.shopButton = null;
+
   
       this.createUI();
       this.createModal();
@@ -19,14 +19,14 @@ export class UIManager {
 
       
   
-      this.scoreText = this.scene.add.text(width / 2, 25, 'Bytes: 0', {
-        fontSize: '30px',
+      this.scoreText = this.scene.add.text(width / 2, 25, 'Bytes:0', {
+        fontSize: '2rem',
         fill: '#00ff00'
       }).setOrigin(0.5, 0.5);
   
       this.fishButton = this.createButton(width / 2, height / 11, 'Phish',
         () => {
-          this.gameManager.bait.body.setVelocityY(100);
+          if(this.gameManager.bait.y === this.gameManager.initialBaitY) this.gameManager.bait.body.setVelocityY(100); //problem in touch screens
           this.gameManager.updateIdleFish();
         },
         () => {
@@ -35,9 +35,12 @@ export class UIManager {
         }
       );
   
-      this.resetButton = this.createButton(width * 9 / 10, height / 20, 'Reset',
+      this.resetButton = this.createButton(width * 9 / 10, height / 20, ` Resets \n   ${this.gameManager.reelCount}`,
         () => {
           this.gameManager.resetBait();
+          console.log(this.gameManager.reelCount)
+
+          this.resetButton.list[1].setText(` Resets \n   ${this.gameManager.reelCount}`)
           this.gameManager.updateIdleFish();
         }
       );
@@ -50,7 +53,7 @@ export class UIManager {
         }
       );
 
-      this.tutButton = this.createButton(width/10, height/9, "How to\n Play", () => {
+      this.tutButton = this.createButton(width/10, height/9, "How to\n play", () => {
         if (this.gameManager.bait.y === this.gameManager.initialBaitY) {
           this.createInstructionsModal();
         }
@@ -64,11 +67,36 @@ export class UIManager {
       const container = this.scene.add.container(0, 0, [bg, text]);
       bg.on('pointerdown', onDown);
       bg.on('pointerup', onUp);
-      return bg;
+      return container;
     }
   
-    updateScore(score) {
-      this.scoreText.setText(`Bytes: ${score}`);
+    updateScore(score, increase) {
+      this.scoreText.setText(`Bytes:${score}`);
+      const { width } = this.scene.game.config;
+
+      if (increase > 0){
+        const effectText = this.scene.add.text(
+          this.scoreText.x + 100, // adjust right of score
+          this.scoreText.y,
+          `+${increase}`,
+          {
+            fontSize: '1rem',
+            fill: increase == 1024 ? '#d4af37' : '#00ff00',
+            fontStyle: 'bold'
+          }
+        ).setOrigin(0, 0.5).setDepth(100); // float above
+  
+        this.scene.tweens.add({
+          targets: effectText,
+          y: effectText.y - 20,
+          alpha: 0,
+          duration: 1000,
+          ease: 'Cubic.easeOut',
+          onComplete: () => {
+            effectText.destroy();
+          }
+        });
+      }
     }
 
 
@@ -85,18 +113,29 @@ export class UIManager {
         .setVisible(false).setDepth(10);
   
       this.modalTitle = this.scene.add.text(centerX, centerY - height / 4, 'SHOP', {
-        fontSize: '64px', fill: '#ffffff'
+        fontSize: '6em', fill: '#ffffff'
       }).setOrigin(0.5).setVisible(false).setDepth(11);
   
       const options = ['Website Spoofing', 'Malicious Link Phishing', 'Spear Phishing'];
+      const details = ['The creation of a fake website that imitates a legitimate one to trick users into entering sensitive \ninformation.', 
+                        'A deceptive tactic where attackers send emails or messages with dangerous links that lead to \nfraudulent sites or trigger malware downloads',
+                        ' A targeted phishing attack that uses personalized information to deceive specific individuals \ninto revealing confidential data or credentials.']
       this.modalOptions = [];
+      this.modalDetails = [];
   
       options.forEach((text, i) => {
         let option = this.scene.add.text(
           centerX - width / 3,
           centerY - height / 10 + i * height / 6,
-          `${text} (${(i + 1) * 1024} bytes)`,
-          { fontSize: '24px', fill: '#00ff00' }
+          `${text}\n (${(i + 1) * 1024} bytes)`,
+          { fontSize: '2em', fill: '#00ff00' }
+        ).setInteractive().setVisible(false).setDepth(11);
+
+        let detail = this.scene.add.text(
+          centerX - width / 3,
+          centerY - height / 20 + i * height / 6,
+          `${details[i]}\n`,
+          { fontSize: '1.5em', fill: '#00ff00' }
         ).setInteractive().setVisible(false).setDepth(11);
   
         option.on('pointerover', () => {
@@ -111,9 +150,9 @@ export class UIManager {
   
         option.on('pointerdown', () => {
           if (this.gameManager.totalScore >= (i + 1) * 1024) {
-            this.perks[i] = true;
+            this.gameManager.totalScore -= (i + 1) * 1024
+            this.gameManager.perks[i] = true;
           } else {
-
             this.scene.tweens.add({
               targets: option,
               duration: 50,
@@ -123,12 +162,11 @@ export class UIManager {
 
               ease: "Sine.easeInOut",
             });
-            // tweeny.play()
-            // console.log(tweeny.isPlaying())
           }
         });
   
         this.modalOptions.push(option);
+        this.modalDetails.push(detail);
       });
   
       this.closeButton = this.scene.add.text(centerX + width / 3, centerY - height / 4, 'X', {
@@ -143,20 +181,22 @@ export class UIManager {
       this.modalBackground.setVisible(true);
       this.modalTitle.setVisible(true);
       this.modalOptions.forEach(o => o.setVisible(true));
+      this.modalDetails.forEach(d => d.setVisible(true));
       this.closeButton.setVisible(true);
       this.scene.time.timeScale = 0;
-      this.fishButton.disableInteractive();
-      this.tutButton.disableInteractive();
+      this.fishButton.list[0].disableInteractive();
+      this.tutButton.list[0].disableInteractive();
     }
   
     closeModal() {
       this.modalBackground.setVisible(false);
       this.modalTitle.setVisible(false);
       this.modalOptions.forEach(o => o.setVisible(false));
+      this.modalDetails.forEach(d => d.setVisible(false));
       this.closeButton.setVisible(false);
       this.scene.time.timeScale = 1;
-      this.fishButton.setInteractive();
-      this.tutButton.setInteractive();
+      this.fishButton.list[0].setInteractive();
+      this.tutButton.list[0].setInteractive();
     }
 
     createInstructionsModal() {
@@ -166,21 +206,21 @@ export class UIManager {
       // Dark semi-transparent background
       this.instructionsBackground = this.scene.add.graphics()
         .fillStyle(0x000000, 0.9)
-        .fillRect(centerX - width * 3 / 8, centerY - height / 3, width * 3 / 4, height * 2 / 3)
+        .fillRect(centerX - width * 3 / 8, centerY - height / 2.5, width * 3 / 4, height * 2 / 2.5)
         .lineStyle(4, 0x00ff00)
-        .strokeRect(centerX - width * 3 / 8, centerY - height / 3, width * 3 / 4, height * 2 / 3)
+        .strokeRect(centerX - width * 3 / 8, centerY - height / 2.5, width * 3 / 4, height * 2 / 2.5)
         .setDepth(10);
     
       // Title
-      this.instructionsTitle = this.scene.add.text(centerX, centerY - height / 4, 'HOW TO PLAY', {
-        fontSize: '48px',
+      this.instructionsTitle = this.scene.add.text(centerX, centerY - height / 3 + 15, 'HOW TO \n PLAY', {
+        fontSize: '6em',
         fill: '#ffffff'
       }).setOrigin(0.5).setDepth(11);
     
       // Instructions text block
       const instructionsText = 
-        "🪝 Tap 'Phish' to drop your bait!\n\n" +
-        "💰 Earn Bytes by catching fish.\n\n" +
+        "\n\n\n\n🪝 Tap 'Phish' to drop your bait!\n\n\n\n" +
+        "💰 Earn Bytes by catching fish.\n\n\n\n" +
         "🛍️ Use Bytes to unlock new phishing techniques.";
     
       this.instructionsBody = this.scene.add.text(
@@ -188,7 +228,7 @@ export class UIManager {
         centerY,
         instructionsText,
         {
-          fontSize: '48px',
+          fontSize: '3em',
           fill: '#00ff00',
           align: 'center',
           wordWrap: { width: width * 0.6 }
@@ -196,7 +236,7 @@ export class UIManager {
       ).setOrigin(0.5).setDepth(11);
     
       // Close button
-      this.instructionsCloseButton = this.scene.add.text(centerX + width / 3, centerY - height / 4, 'X', {
+      this.instructionsCloseButton = this.scene.add.text(centerX + width / 3, centerY - height / 3 + 15, 'X', {
         fontSize: '48px',
         fill: '#ff0000'
       }).setOrigin(0.5).setInteractive().setDepth(11);
@@ -206,8 +246,8 @@ export class UIManager {
         this.instructionsTitle.setVisible(false);
         this.instructionsBody.setVisible(false);
         this.instructionsCloseButton.setVisible(false);
-        this.fishButton.setInteractive();
-        this.tutButton.setInteractive();
+        this.fishButton.list[0].setInteractive();
+        this.shopButton.list[0].setInteractive();
         this.scene.time.timeScale = 1;
       });
     
@@ -217,8 +257,8 @@ export class UIManager {
       this.instructionsBody.setVisible(true);
       this.instructionsCloseButton.setVisible(true);
       this.scene.time.timeScale = 0;
-      this.fishButton.disableInteractive();
-      this.tutButton.disableInteractive();
+      this.fishButton.list[0].disableInteractive();
+      this.shopButton.list[0].disableInteractive();
     }
     
   }
--- gameManager.js
+++ gameManager.js
@@ -9,8 +9,10 @@ export class GameManager {
       this.setupBoatAndBait();
       this.spawnFish();
       this.perks = [false,false,true];
+      this.resets = 3;
     }
-  
+
+
     setupBoatAndBait() {
       const { width, height } = this.scene.game.config;
       this.boat = this.scene.physics.add.sprite(width / 2, height / 6, 'boat').setImmovable(true);
@@ -20,14 +22,14 @@ export class GameManager {
     }
   
     spawnFish() {
-      const height = window.innerHeight;
+      const { width, height } = this.scene.game.config;
       this.fishes = this.scene.physics.add.group();
       const sizeFactors = [0.8, 1, 1.2, 1.4, 1.6];
   
       for (let level = 1; level <= 3; level++) {
         for (let i = 0; i < 20; i++) {
           let x = Phaser.Math.Between(20, this.scene.game.config.width - 20);
-          let y = Phaser.Math.Between(level * height / 4 + 10, (level + 1) * height / 4 - 10);
+          let y = Phaser.Math.Between(level * height / 4 + 20, (level + 1) * height / 4 - 20);
           let type = Phaser.Math.Between(1, 4);
           
           let scaleIndex = Phaser.Math.Between(0, 4);
@@ -36,6 +38,8 @@ export class GameManager {
           fish.type = type;
           fish.setBounce(1);
           fish.setCollideWorldBounds(true);
+          fish.body.onWorldBounds = true;
+          fish.body.setBoundsRectangle(new Phaser.Geom.Rectangle(100, 100, 600, 1400));
           fish.depthLevel = level;
           fish.score = sizeFactors[scaleIndex] * 100 * level;
           fish.moveChance = Phaser.Math.FloatBetween(0, 1);
@@ -46,30 +50,28 @@ export class GameManager {
     }
   
     resetBait() {
-      this.bait.body.enable = false;
-      this.scene.tweens.add({
-        targets: this.bait,
-        y: this.initialBaitY,
-        ease: 'Linear',
-        duration: 1000,
-        onComplete: () => (this.bait.body.enable = true)
-      });
-      this.updateIdleFish();
-      this.fishes.children.iterate(f => f.moveChance = Phaser.Math.FloatBetween(0, 1));
-      this.reelCount--;
-      this.isGameOver = CheckGameOver();
+      if (this.bait.y !== this.initialBaitY){
+        this.bait.body.enable = false;
+        this.scene.tweens.add({
+          targets: this.bait,
+          y: this.initialBaitY,
+          ease: 'Linear',
+          duration: 1000,
+          onComplete: () => (this.bait.body.enable = true)
+        });
+        this.updateIdleFish();
+        this.fishes.children.iterate(f => f.moveChance = Phaser.Math.FloatBetween(0, 1));
+        this.reelCount -= 1
+      }
     }
   
     updateIdleFish() {
-      const baitLevel = this.getDepthLevel(this.bait.y);
       this.fishes.children.iterate(fish => {
         fish.setVelocity(Phaser.Math.Between(-50, 50), 0);
       });
     }
   
     update() {
-
-     
       if (this.bait.y < this.initialBaitY) {
         this.bait.setVelocityY(0);
         this.bait.y = this.initialBaitY;
@@ -85,7 +87,7 @@ export class GameManager {
       {
         this.fishes.children.iterate(fish => {
           if (!fish.active || fish.depthLevel !== baitLevel) return;
-    
+          
           let threshold = 0.3 + (fish.depthLevel - 1) * 0.3;
           if (fish.moveChance > threshold && this.bait.body.velocity.y === 0) {
             let angle = Phaser.Math.Angle.Between(fish.x, fish.y, this.bait.x, this.bait.y);
@@ -95,15 +97,16 @@ export class GameManager {
     
             if (Phaser.Math.Distance.Between(fish.x, fish.y, this.bait.x, this.bait.y) < 20) {
               this.totalScore += fish.score;
-              this.scene.uiManager.updateScore(this.totalScore);
+              this.scene.uiManager.updateScore(this.totalScore, fish.score);
               fish.disableBody(true, true);
               this.applySpawnPhishes(fish);
               const emitter = this.scene.add.particles(fish.x, fish.y, 'spark', {
-                frame: 'white',
-                scale: 0.03,
+                scale: fish.score == 1024 ? 0.04 : 0.03,
                 angle: { min: 0, max: 360 },
-                speed: 200,
-                lifespan:100,
+                speed: fish.score == 1024 ? 400 : 200,
+                lifespan:fish.score == 1024 ? 800 : 100,
+                tint: fish.score == 1024 ? 0xFFD700 : 0xffffff , // 💛 Gold if 1024, white otherwise
+                quantity: fish.score == 1024 ? 100 : 10
               });
   
               this.scene.time.delayedCall(400, () => {
@@ -112,21 +115,14 @@ export class GameManager {
             }
           }
         });
-        this.isGameOver = this.CheckGameOver();
+        if(((this.reelCount == 0 && this.totalScore < this.goalScore) || (this.totalScore >= this.goalScore))){
+
+        }
       }
       
       
     }
 
-
-    CheckGameOver()
-    {
-      if((this.reelCount == 0 && this.totalScore < this.goalScore) || (this.totalScore >= this.goalScore))
-      {
-        return true;
-      } 
-      return false;
-    }
 
     applyProbBoost()
     {
@@ -134,7 +130,6 @@ export class GameManager {
       {
         this.fishes.iterate(fish => fish.moveChance += 0.15);
         this.perks[0] = !this.perks[0];
-
       }
     }
 
@@ -175,28 +170,27 @@ export class GameManager {
     {
       const { width, height } = this.scene.game.config;
 
-      if(this.perks[2])
-      {
+      if(this.perks[2]){
         let x = Phaser.Math.Between(20, this.scene.game.config.width - 20);
-      let y = Phaser.Math.Between(3 * height / 4 + 10, (3 + 1) * height / 4 - 10);
-      let type = Phaser.Math.Between(1, 4);
-  
-      
-      let fish = this.scene.physics.add.sprite(x, y, `cursorFish ${type}`)
-        .setScale((type === 1 || type === 4) ? 0.06 * 2 : 0.01 * 2);
-      fish.type = type;
-      fish.setBounce(1);
-      fish.setCollideWorldBounds(true);
-      fish.depthLevel = 3;
-      fish.score = 2 * 100 * 3;
-      fish.moveChance = 1;
-      fish.setVelocity(Phaser.Math.Between(-50, 50), 0);
-      fish.setTint("0xd4af37");
-      this.fishes.add(fish);
+        let y = Phaser.Math.Between(3 * height / 4 + 10, (3 + 1) * height / 4 - 10);
+        let type = Phaser.Math.Between(1, 4);
+    
+        
+        let fish = this.scene.physics.add.sprite(x, y, `cursorFish ${type}`)
+          .setScale((type === 1 || type === 4) ? 0.06 * 2 : 0.01 * 2);
+        fish.type = type;
+        fish.setBounce(1);
+        fish.setCollideWorldBounds(true);
+        fish.depthLevel = 3;
+        fish.score = 1024;
+        fish.moveChance = 1;
+        fish.setVelocity(Phaser.Math.Between(-50, 50), 0);
+        fish.setTint("0xd4af37");
+        this.fishes.add(fish);
 
-      this.perks[2] = !this.perks[2];
+        this.perks[2] = !this.perks[2];
       } 
-      
+  
 
     }
   
